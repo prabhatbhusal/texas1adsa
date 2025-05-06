10c5c2049d180e3d45c11af0a232baef58a811f0
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-05-06 12:45:39 +0545

    Adding Files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link';
 import React from 'react'
 import { Button } from './ui/button';
 import { ArrowRight } from 'lucide-react';
-
+import { FooterItems } from '@/lib/constants/data';
 const Footer = () => {
   return (
     <section className="relative ">
       {/* {Background Clip part */}
       <div></div>
       {/* {main Footer Part} */}
-      <div className="bg-green-500 flex flex-col px-[100px]">
-        <div className="flex justify-between items-center">
+      <div className="bg-green-500 flex flex-col px-[100px] ">
+        <div className="flex justify-between items-center py-10">
           <h2>Contact</h2>
           <div className="flex gap-5">
             <Link href="#">
@@ -29,7 +29,32 @@ const Footer = () => {
           </div>
         </div>
         <hr className="w-full" />
-        <div></div>
+        <div>
+          <div className="lg:grid-cols-7 md:grid-cols-4 sm:grid-cols-2 grid-cols-1 grid py-10">
+            {FooterItems.map((items, idx) => (
+              <div className="flex flex-col gap-5" key={idx}>
+                <h2>{items.title}</h2>
+                <div className="flex flex-col gap-3">
+                  {items.itemsmenu.map((menuItem, index) => (
+                    <p key={index}>{menuItem.item}</p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          <hr className="w-full" />
+          <div className="py-10">
+            <div>
+              <div>
+
+              </div>
+            </div>
+            <div>
+              <p></p>
+              <p></p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );

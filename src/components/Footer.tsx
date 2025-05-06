56a6ad516f0a83325d65045da1ba638f0eb4dc89
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-05-06 11:34:40 +0545

    Adding Files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,38 @@
+import Link from 'next/link';
 import React from 'react'
+import { Button } from './ui/button';
+import { ArrowRight } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <section>
-      
+    <section className="relative ">
+      {/* {Background Clip part */}
+      <div></div>
+      {/* {main Footer Part} */}
+      <div className="bg-green-500 flex flex-col px-[100px]">
+        <div className="flex justify-between items-center">
+          <h2>Contact</h2>
+          <div className="flex gap-5">
+            <Link href="#">
+              <Button size="lg" className="flex gap-5 items-center">
+                Schedule free consultation <ArrowRight />
+              </Button>
+            </Link>
+            <Link href="#">
+              <Button
+                size="lg"
+                className="flex gap-5 items-center text-red-800"
+              >
+                Request an estimate <ArrowRight />
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <hr className="w-full" />
+        <div></div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Footer

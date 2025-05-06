09f080b574f5557ccb3975babb059adb4cb5893c
prabhatbhusal <prabhatbhusal777@gmail.com>
2025-05-06 17:47:26 +0545

    Adding Files

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -10,7 +10,17 @@ const page = () => {
         content="At Texas Ground Works, we offer full-scale commercial and residential landscaping services tailored to enhance the beauty, function, and value of your outdoor spaces. From expert design and construction to year-round maintenance, irrigation systems, tree care, fencing, and custom features — our experienced team delivers reliable, high-quality solutions built to last. "
       />
       <div className="">
-
+        <div className="flex-1">
+          <Image src="/Gallery/image1.png" alt="alt" width={50} height={50} />
+        </div>
+        <div className="flex-1">
+          <div className="flex-1/2">
+            <Image src="/Gallery/image1.png" alt="alt" width={50} height={50} />
+          </div>
+          <div className="flex-1/2">
+            <Image src="/Gallery/image1.png" alt="alt" width={50} height={50} />
+          </div>
+        </div>
       </div>
     </section>
   );
